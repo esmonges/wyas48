9490a605b1307de2860570b6dadbf7a2cc6961cf
TED <edward.smongeski@gmail.com>
2015-09-07 23:31:55 -0400

    first commit with initial parsing

---
+++ Main.hs
@@ -0,0 +1,44 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+data LispVal = Atom String
+        | List [LispVal]
+        | DottedList [LispVal] LispVal
+        | Number Integer
+        | String String
+        | Bool Bool
+
+main :: IO ()
+main = do
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
+    --name <- getLine
+    --putStrLn(name)
+    --args <- getArgs
+    --putStrLn("Hello, " ++ args !! 0 ++ " and " ++ args !! 1)
+    --putStrLn("Result is " ++ show(read(args !! 0) + read(args !! 1)))
+
+--TODO: How does this typing work?
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+--TODO: How does this typing work?
+spaces :: Parser ()
+spaces = skipMany1 space
+
+--vowel :: Parser Char
+--vowel = oneOf "aeiou"
+
+readExpr :: String -> String
+--TODO: How does parse work? look at types
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
